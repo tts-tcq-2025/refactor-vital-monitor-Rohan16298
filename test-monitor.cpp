297848bc4e505fa2481751eee91086a1a7c708a0
Rohan16298 <urk3hyd@bosch.com>
2025-09-29 09:44:24 +0530

    Update test-monitor.cpp

--- test-monitor.cpp
+++ test-monitor.cpp
@@ -1,7 +1,26 @@
 #include <gtest/gtest.h>
-#include "./monitor.h"
+#include "monitor.h"
 
-TEST(Monitor, NotOkWhenAnyVitalIsOffRange) {
-  ASSERT_FALSE(vitalsOk(99, 102, 70));
-  ASSERT_TRUE(vitalsOk(98.1, 70, 98));
+TEST(Monitor, TemperatureOutOfRange) {
+    ASSERT_FALSE(vitalsOk(94.9, 70, 95));
+    ASSERT_FALSE(vitalsOk(102.1, 70, 95));
 }
+
+TEST(Monitor, PulseRateOutOfRange) {
+    ASSERT_FALSE(vitalsOk(98, 59, 95));
+    ASSERT_FALSE(vitalsOk(98, 101, 95));
+}
+
+TEST(Monitor, Spo2OutOfRange) {
+    ASSERT_FALSE(vitalsOk(98, 70, 89));
+}
+
+TEST(Monitor, AllVitalsOk) {
+    ASSERT_TRUE(vitalsOk(98.6, 70, 95));
+}
+
+TEST(Monitor, BoundaryValues) {
+    ASSERT_TRUE(vitalsOk(95, 60, 90));
+    ASSERT_TRUE(vitalsOk(102, 100, 90));
+}
+
